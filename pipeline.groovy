5390feb331e1f37ecfcec6e99201416dee3dfa36
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-11-25 17:26:45 +0530

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                eho 'Hello World'
+            }
+        }
+    }
+
+    post {
+    always {
+        // Print the job name and build number before triggering the next pipeline
+        echo "Job Name: ${JOB_NAME}"
+        echo "Build Number: ${BUILD_NUMBER}"
+        
+        build job: 'heal-pipeline', parameters: [
+                string(name: 'PIPELINE_NAME', value: "${JOB_NAME}"),
+                string(name: 'BUILD_NUMBER', value: "${BUILD_NUMBER}")
+            ], wait: false
+    }
+}
+}
