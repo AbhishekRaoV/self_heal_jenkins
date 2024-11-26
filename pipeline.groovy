b991b8cb6b1f7478078cbe919c3cc6cdda103a4b
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-11-26 12:12:18 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,16 +9,16 @@ pipeline {
         }
     }
 
-    post {
-        failure {
-            // Print the job name and build number before triggering the next pipeline
-            echo "Job Name: ${JOB_NAME}"
-            echo "Build Number: ${BUILD_NUMBER}"
+    // post {
+    //     failure {
+    //         // Print the job name and build number before triggering the next pipeline
+    //         echo "Job Name: ${JOB_NAME}"
+    //         echo "Build Number: ${BUILD_NUMBER}"
             
-            build job: 'heal-pipeline', parameters: [
-                    string(name: 'PIPELINE_NAME', value: "${JOB_NAME}"),
-                    string(name: 'BUILD_NUMBER', value: "${BUILD_NUMBER}")
-                ], wait: false
-        }
-    }
+    //         build job: 'heal-pipeline', parameters: [
+    //                 string(name: 'PIPELINE_NAME', value: "${JOB_NAME}"),
+    //                 string(name: 'BUILD_NUMBER', value: "${BUILD_NUMBER}")
+    //             ], wait: false
+    //     }
+    // }
 
